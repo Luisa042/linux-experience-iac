151590b6703e312128998fffb0e7ca9284d7f898
Luisa <luisa.santos042@gmail.com>
2022-08-15 01:41:10 +0000

    bug fix

--- iac.sh
+++ iac.sh
@@ -15,17 +15,17 @@ groupadd GRP_SEC
 
 echo "Creating users..."
 
-useradd carlos -m -c "Carlos" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_ADM
-useradd maria -m -c "Maria" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_ADM
-useradd joao -m -c "João" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_ADM
+useradd carlos -m -c "Carlos" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_ADM
+useradd maria -m -c "Maria" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_ADM
+useradd joao -m -c "João" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_ADM
 
-useradd debora -m -c "Débora" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_VEN
-useradd sebastiana -m -c "Sebastiana" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_VEN
-useradd roberto -m -c "Roberto" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_VEN
+useradd debora -m -c "Débora" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_VEN
+useradd sebastiana -m -c "Sebastiana" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_VEN
+useradd roberto -m -c "Roberto" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_VEN
 
-useradd josefina -m -c "Josefina" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_SEC
-useradd amanda -m -c "Amanda" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_SEC
-useradd rogerio -m -c "Rogério" -s /bin/bash -p ${openssl passwd -crypt Senha321} -G GRP_SEC
+useradd josefina -m -c "Josefina" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_SEC
+useradd amanda -m -c "Amanda" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_SEC
+useradd rogerio -m -c "Rogério" -s /bin/bash -p $(openssl passwd -crypt Senha321) -G GRP_SEC
 
 echo "Editing directories permissions..."
 
